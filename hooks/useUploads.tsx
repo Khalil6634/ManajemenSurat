4e62e951157e142efc429e6df8a9c0bb22211d87
Khalil6634 <sawanganpc@gmail.com>
2026-05-15 01:34:00 +0700

    update useUpload to upload in permanent directory

--- hooks/useUploads.tsx
+++ hooks/useUploads.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system/legacy';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const useUploads = () => {
   const [selectedFile, setSelectedFile] = useState<DocumentPicker.DocumentPickerAsset | null>(null);
@@ -14,8 +16,32 @@ const useUploads = () => {
       });
 
       if (!result.canceled) {
+        if (result.assets && result.assets.length > 0) {
+          const permanentdirectory = FileSystem.documentDirectory + result.assets[0].name;
+
+          try {
+            await FileSystem.copyAsync({
+              from: result.assets[0].uri,
+              to: permanentdirectory,
+            });
+
+            const fileData = {
+              name: result.assets[0].name,
+              uri: permanentdirectory,
+              size: result.assets[0].size,
+              mimeType: result.assets[0].mimeType,
+              lastModified: result.assets[0].lastModified,
+            };
+            await AsyncStorage.setItem('uploadedFile', JSON.stringify(fileData));
+            setMessage('File copied to permanent directory successfully!');
+            
+          } catch (error) {
+            console.error('Error copying file: ', error);
+            setMessage('Error occurred while copying file.');
+          }
+        }
+
         setSelectedFile(result.assets[0]);
-        setMessage('File selected successfully!');
         setIsUploading(true);
       }
     } catch (err) {
